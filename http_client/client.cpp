50bc74ab49627ec3769b3027548906ebfd2f496d
Dahlia <dahliachehata@gmail.com>
2018-11-20 03:43:06 +0200

    client modifications

--- http_client/client.cpp
+++ http_client/client.cpp
@@ -23,6 +23,7 @@
 
 
 #define MAXDATA 1024
+#define PORT "80"
 
 using namespace std;
 
@@ -58,30 +59,40 @@ void send_file_to_server(const int sockfd,const string &filename)
     sprintf(file_size, "%jd", file_stat.st_size);
 
     // send file size
-    bytes = send(sockfd, file_size, MAXDATA, 0);
+    bytes = send(sockfd, file_size, sizeof(file_size), 0);
     if (bytes < 0)
     {
         fprintf(stderr, "Error on sending file size %s", strerror(errno));
         exit(EXIT_FAILURE);
     }
-
     offset = 0;
     remain_data = file_stat.st_size;
     cout << "file size = " << file_size << endl;
-   while (((sent_bytes = sendfile(sockfd, fd, &offset, MAXDATA)) > 0) && (remain_data > 0)) {
+//    while (((sent_bytes = sendfile(sockfd, fd, &offset, MAXDATA)) > 0) && (remain_data > 0)) {
+//        remain_data -= sent_bytes;
+//        cout << "remain data = " << remain_data << endl;
+//    }
+
+    while (((remain_data > MAXDATA) && (sent_bytes = sendfile(sockfd, fd, &offset, MAXDATA)) > 0)  ) {
       remain_data -= sent_bytes;
       cout << "remain data = " << remain_data << endl;
+    }
+    if (remain_data)
+    {
+        sendfile(sockfd, fd, &offset, remain_data);
+        cout << "remain data = 0" << endl;
     }
 }
 void rcv_file_from_server(const int sockfd,const string &filename)
 {
-char httpmsg[MAXDATA];
+    char httpmsg[MAXDATA];
     FILE *received_file;
     int file_size, remain_data, bytes;
 
     // receive file size
     recv(sockfd, httpmsg, MAXDATA, 0);
-    file_size = atoi(httpmsg);
+    file_size=atoi(httpmsg);
+
     received_file = fopen(filename.c_str(), "w");
     if (received_file == NULL) {
         fprintf(stderr, "Failed to open file %s\n", strerror(errno));
@@ -112,22 +123,23 @@ char httpmsg[MAXDATA];
         }
     }
 
-fclose(received_file);
+  fclose(received_file);
 }
 void send_header_line (const int fd,const string & method,const string &filename)
 {
+    types_manager tp;
     string msg = method + " " + filename + " HTTP/1.0\r\n";
-    msg += "Content-Type: " + types_manager().generate_file_header(filename)+ "text/html\r\n";
+    msg += "Content-Type: " + get_file_type(filename)+ "text/html\r\n";
     msg += "\r\n";
     //used when socket in a connected state
-    send(fd, msg.c_str(), MAXDATA, 0);
+    send(fd, msg.c_str(), strlen(msg.c_str()), 0);
 }
-//std::string get_file_type(std::string filename)
-//{
-//    if (filename.compare(filename.size() - 3, 3, "jpg") == 0) return "image/jpg";
-//    else if (filename.compare(filename.size() - 3, 4, "html") == 0) return "text/html";
-//    else return "text/plain";
-//}
+std::string get_file_type(std::string filename)
+{
+    if (filename.compare(filename.size() - 3, 3, "jpg") == 0) return "image/jpg";
+    else if (filename.compare(filename.size() - 3, 4, "html") == 0) return "text/html";
+    else return "text/plain";
+}
 int main(int argc, char *argv[]) {
     string command;
     int sockfd, status;
@@ -135,17 +147,22 @@ int main(int argc, char *argv[]) {
     struct addrinfo *res;
 
     // Open the TCPConnection
-    if (argc != 3) {
-        fprintf(stderr,"Insufficient params\n");
+    if (argc < 2) {
+        fprintf(stderr,"Insufficient parameters\n");
         exit(1);
     }
 
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
+    char * port_number =(char*) PORT;
+    if(argc==3)
+    {
+        port_number=argv[2];
+    }
 
     // getaddrinfo() system call
-    if ((status = getaddrinfo(argv[1], argv[2], &hints, &res)) != 0) {
+    if ((status = getaddrinfo(argv[1], port_number, &hints, &res)) != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
         exit(1);
     }
@@ -158,11 +175,6 @@ int main(int argc, char *argv[]) {
 
     clock_t begin = clock();
 
-    // connect() system call
-    if (connect(sockfd, res->ai_addr, res->ai_addrlen) == -1) {
-        perror("client: connect");
-        exit(1);
-    }
 
     // Process the commands
     ifstream commandsfile ("commands.txt", ios::in);
